5e31983a8f81c311e91b2906ad555b19ac5fca49
dkqydhd <126450338+dkqydhd@users.noreply.github.com>
2023-04-28 17:57:40 +0900

    0428

--- PetProject3/src/jsp/Recipe/Ingredients_Img.java
+++ PetProject3/src/jsp/Recipe/Ingredients_Img.java
@@ -56,62 +56,64 @@ public class Ingredients_Img{
 		//sql문장
 		String sql = "";  
 //		4 select 명령문을 생성 및 실행
-		sql = "select * from Ingredients_img";
-//		sql = "select * from Ingredients_img where i_name=?"  
-		Statement stmt = con.createStatement(); //web의 session같은 존재
-		ResultSet result = stmt.executeQuery(sql);
-
-//		5. update 명령문을 생성 및 실행
-//		sql = "UPDATE Ingredients_img SET color = ? WHERE length =?";
-//		PreparedStatement stmt = con.prepareStatement(sql);
+//		sql = "select * from Ingredients_img";
+////		sql = "select * from Ingredients_img where i_name=?"  
+//		Statement stmt = con.createStatement(); //web의 session같은 존재
+//		ResultSet result = stmt.executeQuery(sql);
+////
+////		5. update 명령문을 생성 및 실행
+////		sql = "UPDATE Ingredients_img SET color = ? WHERE length =?";
+////		PreparedStatement stmt = con.prepareStatement(sql);
+////		
+////		stmt.setString(1,"#74B230");
+////		stmt.setFloat(2,400);
+////		int result = stmt.executeUpdate();
+////	    System.out.println("Update 결과 : " + result);
 //		
-//		stmt.setString(1,"#74B230");
-//		stmt.setFloat(2,400);
-//		int result = stmt.executeUpdate();
-//	    System.out.println("Update 결과 : " + result);
-		
-		while (result.next()) {
-			String image_url = result.getString("image_url");
-            File imageFile = new File(image_url);
-			String i_name = result.getString("i_name");
-			String color = result.getString("color");
-			float length = result.getFloat("length");
-			float weight = result.getFloat("weight");
-			BufferedImage image = ImageIO.read(imageFile);
-            ImageIO.write(image, "jpg", imageFile);
-					  						
-//		데이터 출력
-			System.out.println("i_name: " + i_name);
-			System.out.println("color: " + color);
-			System.out.println("length: " + length+"cm");
-			System.out.println("weight: " + weight+"g");
-			System.out.println("image_url:" + image_url);
-			} 
-		 
-//		4-1. 4번 resultset close
-		result.close();  
+//		while (result.next()) {
+//			String image_url = result.getString("image_url");
+//			File imageFile = new File(image_url);
+//			String i_name = result.getString("i_name");
+//			String color = result.getString("color");
+//			float length = result.getFloat("length");
+//			float weight = result.getFloat("weight");
+//			String image_name = result.getString("image_name");
+//			BufferedImage image = ImageIO.read(imageFile);
+//			ImageIO.write(image, "jpg", imageFile);
+//				  						
+//////		데이터 출력
+//			System.out.println("i_name: " + i_name);
+//			System.out.println("color: " + color);
+//			System.out.println("length: " + length+"cm");
+//			System.out.println("weight: " + weight+"g");
+//			System.out.println("image_name:" + image_name);
+//			System.out.println("image_url:" + image_url);
+//		} 
+//		 
+////		4-1. 4번 resultset close
+//		result.close();  
 		
 //		6. insert 명령문을 생성 및 실행
-//		sql = "insert into Ingredients_img(i_name, color, length, weight,image_url )" +
-//			  "values(?,?,?,?,?)";
-//		PreparedStatement stmt = con.prepareStatement(sql);
-//		
-////		6-1 이미지 파일 저장하기 위한 준비
-//		String filename = "C://Users//anyware//Pictures//배추.jpg";
-
-////		7 데이터 삽입(레코드세트)
-//		stmt.setString(1, "배추");
-//		stmt.setString(2, "#F76C35" );
-//		stmt.setFloat(3, 400);
-//		stmt.setFloat(4, 200);
-//        stmt.setString(5, filename);
-//		
-//        stmt.executeUpdate();
-//        System.out.println("이미지 경로를 저장하였습니다.");
+		sql = "insert into Ingredients_img(i_name, color, length, weight, image_name, image_url )" +
+			  "values(?,?,?,?,?,?)";
+		PreparedStatement stmt = con.prepareStatement(sql);
+		
+//		6-1 이미지 파일 저장하기 위한 준비
+		String filename = "C://Users//anyware//Pictures//.jpg";
+		
+//		7 데이터 삽입(레코드세트)
+		stmt.setString(1, "배추");
+		stmt.setString(2, "#F76C35" );
+		stmt.setFloat(3, 400);
+		stmt.setFloat(4, 200);
+		stmt.setString(5, "배추");
+        stmt.setString(6, filename);
+        stmt.executeUpdate();
+        System.out.println("이미지 경로를 저장하였습니다.");
 //		8. 4 or 5 close
-//		stmt.close();
+		stmt.close();
 //		9. 3번 close
-//		con.close();	 
+		con.close();	 
 	}}
 
     
--- PetProject3/src/jsp/Recipe/test_file.java
+++ PetProject3/src/jsp/Recipe/test_file.java
@@ -12,18 +12,18 @@ import java.util.List;
 
 public class test_file {
     public static void main(String[] args) {
-        String csvFilePath = "C:\\Users\\anyware\\Desktop\\밥사조 프로젝트\\table test\\ingredient_quantities.csv";
+        String csvFilePath = "C:\\Users\\anyware\\Desktop\\밥사조 프로젝트\\table test\\test.csv";
         String line = "";
-        String cvsSplitBy = " ";
+        String cvsSplitBy = "\\|";
         List<String> ingredients = new ArrayList<>();
-
+        
         try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
             while ((line = br.readLine()) != null) {
                 String[] recipe = line.split(cvsSplitBy);
                 String name = recipe[0];    
                 String number = recipe[1];
                 ingredients.add(name);
-            }
+            }System.out.println(ingredients);
             FileOutputStream fos = new FileOutputStream("C:\\Users\\anyware\\Desktop\\밥사조 프로젝트\\table test\\ingredient1.txt");
             Writer writer = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
             for (int i = 0; i < ingredients.size(); i++) {
